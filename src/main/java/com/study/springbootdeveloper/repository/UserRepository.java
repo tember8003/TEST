20a31cd49f7adb924bf08b463dee3ea07a92c731
yuchan <yuchan8003@naver.com>
2025-11-04 17:11:40 +0900

    Fix:Repository

--- src/main/java/com/study/springbootdeveloper/repository/UserRepository.java
+++ src/main/java/com/study/springbootdeveloper/repository/UserRepository.java
@@ -9,10 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
-    boolean existsByEmail(String email);
-
     Optional<User> findByLoginId(String loginId);
 
     boolean existsByLoginId(String loginId);
