2a84a49d700996756b31f8f2e84451a70206a6f9
yuchan <yuchan8003@naver.com>
2025-11-04 17:00:09 +0900

    Feat:AdminInitializer

---
+++ src/main/java/com/study/springbootdeveloper/config/AdminUserInitializer.java
@@ -0,0 +1,35 @@
+package com.study.springbootdeveloper.config;
+
+import com.study.springbootdeveloper.domain.User;
+import com.study.springbootdeveloper.repository.UserRepository;
+import com.study.springbootdeveloper.type.UserRole;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AdminUserInitializer {
+
+    @Value("${SPRING_SECURITY_USER_NAME}")
+    private String adminUsername;
+
+    @Value("${SPRING_SECURITY_USER_PASSWORD}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner createAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (!userRepository.existsByLoginId((adminUsername))) {
+                User admin = User.builder()
+                        .loginId(adminUsername)
+                        .password(passwordEncoder.encode(adminPassword))
+                        .role(UserRole.ROLE_ADMIN)
+                        .build();
+                userRepository.save(admin);
+                System.out.println("관리자 계정 생성 완료: " + adminUsername);
+            }
+        };
+    }
+}
--- src/main/java/com/study/springbootdeveloper/repository/UserRepository.java
+++ src/main/java/com/study/springbootdeveloper/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
 }
